e2d606a2681cadb6f31f96f7451100af37576972
pablodelgado26 <158165561+pablodelgado26@users.noreply.github.com>
2024-10-03 09:01:45 -0300

    refactor: alterando servidor para uso de rotas

--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
 import express from "express";
 import { config } from "dotenv";
 
+import routes from "./routes/index.routes.js";
+
 config();
 
 const serverPort = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
+app.use(routes);
 
-app.get("/", (req, res) => {
-  return res.status(200).json({ message: "Hello, World!" });
-});
 
 app.listen(serverPort, () => {
-  console.log(`⚡ Servidor está top só bora http://localhost:${serverPort}`);
+    console.log(`🃏 Servidor está top só bora http://localhost:${serverPort}`);
 });
